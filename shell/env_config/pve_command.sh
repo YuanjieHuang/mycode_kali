88f15e90815324f0f8b4606cc725e8109e68e016
yjie <yjie@kali.lan>
2021-11-14 11:50:08 +0800

    update

--- shell/env_config/pve_command.sh
+++
@@ -1,6 +0,0 @@
-/var/lib/vz/template/iso/
-qm importovf 999  WindowsServer2012.ovf  local-lvm
-pvesh get /nodes/pve/qemu/105/status/current
-pvesh get /cluster/resources
-pvesh create /nodes/pve/qemu/201/status/start
-cat /etc/iscsi/initiatorname.iscsi
